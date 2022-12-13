0668bf5a105685e253a7ffaf20095285021d896e
Vitalii <zhingel87@gmail.com>
2022-12-13 18:10:36 +0200

    [PMNT-1] Edit Users model

--- src/main/java/src/models/User.java
+++ src/main/java/src/models/User.java
@@ -1,5 +1,11 @@
 package src.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class User {
 
     private String firstName;
@@ -8,44 +14,4 @@ public class User {
     private int indivTaxNumber;
     private int phone;
 
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getThirdName() {
-        return thirdName;
-    }
-
-    public void setThirdName(String thirdName) {
-        this.thirdName = thirdName;
-    }
-
-    public int getIndivTaxNumber() {
-        return indivTaxNumber;
-    }
-
-    public void setIndivTaxNumber(int indivTaxNumber) {
-        this.indivTaxNumber = indivTaxNumber;
-    }
-
-    public int getPhone() {
-        return phone;
-    }
-
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
 }
