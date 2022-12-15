37a3295341f185abe9c3f566c26490db008a0522
Vitalii <zhingel87@gmail.com>
2022-12-15 11:22:19 +0200

    [PMNT-1] Edit class App

--- src/main/java/src/App.java
+++ src/main/java/src/App.java
@@ -1,4 +1,32 @@
 package src;
 
+import java.util.Scanner;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println();
+            System.out.println("1. Add User.");
+            System.out.println("2. Add Organization.");
+            // System.out.println("");
+            System.out.println("0. Exit.");
+
+            int command = scanner.nextInt();
+
+            if (command == 1) {
+
+            } else if (command == 2) {
+
+            } else if (command == 0) {
+                System.exit(0);
+            } else {
+                System.err.println("Wrong command.");
+            }
+
+        }
+    }
 }
